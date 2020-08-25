32ce5ba9fbdeaa5dfadea5fb2f7d178036a91447
leoncordell <46611965+leoncordell@users.noreply.github.com>
2020-08-25 13:16:26 +0100

    Remove UserError class

--- src/main/java/com/dwp/users/controller/UserController.java
+++ src/main/java/com/dwp/users/controller/UserController.java
@@ -1,14 +1,12 @@
 package com.dwp.users.controller;
 
 import com.dwp.users.model.User;
-import com.dwp.users.model.UserError;
 import com.dwp.users.service.HeroGatewayService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,7 +28,6 @@ public class UserController {
             @ApiResponse(code = 500, message = "not available!")})
     @GetMapping("/city/London/users/all")
     @ResponseBody
-    @ExceptionHandler(UserError.class)
     public List<User> getAllUsersInCityAndWithinCatchment() {
         return heroGatewayService.retrieveUsers();
     }
